71c9f79763bbabec58d81d8a0c3fdb0e4a9df760
Tsing Gu <mcvector.gu@gmail.com>
2020-05-03 02:16:13 -0400

    Fixed debug layer issue

--- src/Validation.cpp
+++ src/Validation.cpp
@@ -31,7 +31,7 @@ static VKAPI_PTR VkBool32 debugCallback(
     VkDebugUtilsMessageTypeFlagsEXT messageType,
     const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
 {
-    if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
+    if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
     {
         std::cerr << pCallbackData->pMessage << std::endl;
     }
@@ -39,7 +39,6 @@ static VKAPI_PTR VkBool32 debugCallback(
 }
 
 // Call add callback by query the extension
-// TODO: Move to validation utils
 VkResult createDebugUtilsMessenger(
     VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
     const VkAllocationCallbacks* pAllocator,
@@ -70,7 +69,17 @@ void DebugUtilsMessenger::initialize()
     VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
 
-    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT;
+    createInfo.messageType = 
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT 
+        | VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
+        ;
+    createInfo.messageSeverity = 
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+        ;
     createInfo.pfnUserCallback = debugCallback;
 
     assert(createDebugUtilsMessenger(
