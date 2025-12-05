a7248f2ca3855122c3424b581a1dc58a75ecc8c4
qgu <qgu@qgu.io>
2025-12-04 20:36:54 -0500

    Add GetDescType API

--- src/RenderGraph/RenderGraphResourceDesc.h
+++ src/RenderGraph/RenderGraphResourceDesc.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "RenderResource.h"
 #include "RenderResourceManager.h"
+#include "RenderTargetResource.h"
 #include <concepts>
 #include <string_view>
 #include <variant>
@@ -9,13 +10,13 @@
 template <class D>
 concept GraphResourceDesc = requires(const D& d, Muyo::RenderResourceManager* rs)
 {
-    //{ d.name } -> std::convertible_to<std::string_view>;
-    { AllocateImp(d, rs) } -> std::convertible_to<Muyo::IRenderResource*>;
+    { AcquireImp(d, rs) } -> std::convertible_to<Muyo::IRenderResource*>;
+    //{GetDescName(d)} -> std::convertible_to<std::string_view>;
 };
 
 template <GraphResourceDesc D>
 auto Allocate(const D& d, Muyo::RenderResourceManager* renderResourceManager) {
-    return AllocateImp(d, renderResourceManager); // unqualified call → ADL finds it
+    return AcquireImp(d, renderResourceManager); // unqualified call → ADL finds it
 }
 
 template <GraphResourceDesc D>
@@ -34,10 +35,16 @@ namespace Muyo::RenderGraph
         static constexpr size_t STRIDE = sizeof(T);
     };
     template <class T>
-    inline Muyo::IndexBuffer* AllocateImp(const IndexBufferDesc<T>& d, Muyo::RenderResourceManager* renderResourceManager)
+    inline Muyo::IndexBuffer* AcquireImp(const IndexBufferDesc<T>& d, Muyo::RenderResourceManager* renderResourceManager)
     {
         return renderResourceManager->GetIndexBuffer<T>(d.name, std::vector<T>(d.count));
     }
+
+    template <class T>
+    VkDescriptorType GetDescriptorType(const IndexBufferDesc<T>&) {
+        return VK_DESCRIPTOR_TYPE_STORAGE_BUFFER; // or appropriate type
+    }
+
 
     template<class T>
     struct VertexBufferDesc
@@ -48,9 +55,13 @@ namespace Muyo::RenderGraph
     };
 
     template<class T>
-    inline Muyo::VertexBuffer<T>* AllocateImp(const VertexBufferDesc<T>& d, Muyo::RenderResourceManager* renderResourceManager)
+    inline Muyo::VertexBuffer<T>* AcquireImp(const VertexBufferDesc<T>& d, Muyo::RenderResourceManager* renderResourceManager)
     {
         return renderResourceManager->GetVertexBuffer<T>(d.name, std::vector<T>(d.count));
+    }
+    template <class T>
+    VkDescriptorType GetDescriptorType(const VertexBuffer<T>&) {
+        return VK_DESCRIPTOR_TYPE_STORAGE_BUFFER; // or appropriate type
     }
 
     template <class T>
@@ -63,10 +74,28 @@ namespace Muyo::RenderGraph
     };
 
     template <class T>
-    inline Muyo::StorageBuffer<T>* AllocateImp(const StorageBufferDesc<T>& d,
+    inline Muyo::StorageBuffer<T>* AcquireImp(const StorageBufferDesc<T>& d,
                                                Muyo::RenderResourceManager* renderResourceManager)
     {
         return renderResourceManager->GetStorageBuffer<T>(d.name, std::vector<T>(d.count));
+    }
+    template <class T>
+    VkDescriptorType GetDescriptorType(const StorageBufferDesc<T>&) {
+        return VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+    }
+
+    struct RenderTargetDesc
+    {
+        std::string name;
+        VkExtent2D extent{ 0, 0 };
+        VkFormat format{VK_FORMAT_R8G8B8A8_UNORM};
+        uint32_t numMips{1};
+        uint32_t numLayers{1};
+        VkImageUsageFlags usage{VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT};
+    };
+    inline Muyo::RenderTarget* AcquireImp(const RenderTargetDesc& d, Muyo::RenderResourceManager* renderResourceManager)
+    {
+        return renderResourceManager->GetRenderTarget(d.name, d.extent, d.format, d.numMips, d.numLayers, d.usage);
     }
 
     // TODO(qgu): Probalby need to generate known types during compile time
@@ -75,7 +104,8 @@ namespace Muyo::RenderGraph
         IndexBufferDesc<uint16_t>, 
         VertexBufferDesc<Muyo::Vertex>, 
         VertexBufferDesc<Muyo::UIVertex>,
-        StorageBufferDesc<uint8_t> >;
+        StorageBufferDesc<uint8_t>,
+        RenderTargetDesc >;
 }
 
 // Example usage:
